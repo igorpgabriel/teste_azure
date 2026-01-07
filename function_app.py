41e868e7538358a9b40599be9a50a4385936d1c2
Igor Gabriel <81106970+igorpgabriel@users.noreply.github.com>
2026-01-07 10:22:49 -0300

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -5,10 +5,11 @@ from main import funcao_teste
 
 app = func.FunctionApp()
 
-@app.timer_trigger(schedule="0 0 8 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False) 
+@app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('O agendamento está atrasado!')
     
+    logging.info('Iniciando chamada da funcao_teste...')
     funcao_teste()
     logging.info('Script executado com sucesso.')
