2a4723520a0b6d3c93096db8b6e6db0cb6c2b391
Igor Gabriel <81106970+igorpgabriel@users.noreply.github.com>
2026-01-07 09:32:58 -0300

    Update and rename main.py to function_app.py

---
+++ function_app.py
@@ -0,0 +1,14 @@
+import azure.functions as func
+import datetime
+import logging
+from meu_script import minha_funcao
+
+app = func.FunctionApp()
+
+@app.timer_trigger(schedule="0 0 8 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False) 
+def timer_trigger(myTimer: func.TimerRequest) -> None:
+    if myTimer.past_due:
+        logging.info('O agendamento está atrasado!')
+    
+    minha_funcao()
+    logging.info('Script executado com sucesso.')
