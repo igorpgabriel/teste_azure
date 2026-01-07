02a152e3b2ed51ac5ab9c1a2b4fa016d654b891d
Igor Gabriel <81106970+igorpgabriel@users.noreply.github.com>
2026-01-07 09:35:14 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# meu_script.py
+import datetime
+
+def executar_tarefa():
+    agora = datetime.datetime.now()
+    print(f"--- Script executado internamente em: {agora} ---")
+    
+    # Aqui entraria sua lógica real (ex: salvar no banco, scrap, etc.)
+    return True
